13fcb6449f92df05de15de82e8ff9fde079938c1
abderrahmane <abderrahamane7@gmail.com>
2022-02-07 09:57:39 +0100

    Node class added

---
+++ main.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+let n1 = new Node(1)
+let n2 = new Node(2)
+let n3 = new Node(3)
+let n4 = new Node(4)
+let n5 = new Node(5)
+let n6 = new Node(6)
