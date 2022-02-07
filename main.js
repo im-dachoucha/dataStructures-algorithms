3e7dc37f74890e8a3badc7e39797e363097ab6ee
abderrahmane <abderrahamane7@gmail.com>
2022-02-07 09:59:26 +0100

    List class added

--- main.js
+++ main.js
@@ -5,6 +5,38 @@ class Node {
   }
 }
 
+class List {
+  constructor(node) {
+    this.head = node
+  }
+
+  addToEnd(node) {
+    if (!this.head) {
+      this.head = node
+    } else {
+      let n = this.head
+      while (n.next !== null) n = n.next
+      n.next = node
+    }
+  }
+
+  addAtFirst(node) {
+    if (!this.head) this.head = node
+    else {
+      node.next = this.head
+      this.head = node
+    }
+  }
+
+  printList() {
+    let n = this.head
+    while (n) {
+      console.log(n.value)
+      n = n.next
+    }
+  }
+}
+
 let n1 = new Node(1)
 let n2 = new Node(2)
 let n3 = new Node(3)
